a29937b42e61576551191f83914310dfc0dea01a
Nathan Nguyen <nathan.tm.nguyen@gmail.com>
2021-12-20 16:21:20 -0600

    add lottery challenge

---
+++ rev/lottery/README.md
@@ -0,0 +1,7 @@
+# Lottery
+
+## Description
+Come test your luck -- if you're lucky enough, you might get a flag!
+
+## Solution
+TODO
---
+++ rev/lottery/chall.py
@@ -0,0 +1,11 @@
+from random import randint, seed
+
+name = input("What's your name?\n> ")
+
+seed(sum(ord(c) for c in name))
+
+if all(randint(0, 1000000000) == int(input(f"Guess #{i + 1}:\n> ")) for i in range(5)):
+    with open("flag.txt") as f:
+        flag = f.read()
+        print(f"Congratulations, {name}! Here's the flag: {flag}")
+
